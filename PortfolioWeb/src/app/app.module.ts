f3c0e35117cf8d3db53f229550831e67d15aaf09
jctil <jctilatti@gmail.com>
2022-08-23 00:47:58 -0300

    Actualizado el proyecto de angular

---
+++ PortfolioWeb/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HardandsoftskillsComponent } from './components/hardandsoftskills/hardandsoftskills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SobremiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardandsoftskillsComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
